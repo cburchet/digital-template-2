f4267d675ebc12fb0fe77d75e90f30fbb9d97f81
cburchet <cburchet@gmu.edu>
2015-02-11 22:17:04 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ window.onload = function()
 		game.physics.enable(playerPaddle, Phaser.Physics.ARCADE);
 		playerPaddle.scale.setTo(.5, .5);
 		playerPaddle.angle = 90;
-		playerPaddle.anchor.setTo(0.75, 0.75);
+		playerPaddle.anchor.setTo(0.25, 0.25);
 		game.physics.enable('playerPaddle', Phaser.Physics.ARCADE);
 		
 		playerPaddle.body.collideWorldBounds = true;
@@ -72,7 +72,7 @@ window.onload = function()
 		game.physics.enable(computerPaddle, Phaser.Physics.ARCADE);
 		computerPaddle.scale.setTo(.5, .5);
 		computerPaddle.angle = 90;
-		computerPaddle.anchor.setTo(0.25, 0.25);
+		computerPaddle.anchor.setTo(0.75, 0.75);
 		game.physics.enable('computerPaddle', Phaser.Physics.ARCADE);
 		
 		computerPaddle.body.collideWorldBounds = true;
